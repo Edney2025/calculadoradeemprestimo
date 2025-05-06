9600c5b1b662c6d29d437fd5ed2bf58c2b4a711b
Edney2025 <encomendapalotina@gmail.com>
2025-05-06 20:35:28 -0300

    Atualizei o sistema com novas melhorias

---
+++ pages/catalogo/produtos.js
@@ -0,0 +1,5 @@
+[
+    { "id": "1", "category": "automotivo", "subcategory": "autopecas", "name": "Pneu Aro 15", "description": "Pneu de alta performance", "price": "250.00" },
+    { "id": "2", "category": "automotivo", "subcategory": "som-automotivo", "name": "Alto-falante", "description": "Alto-falante de 6 polegadas", "price": "120.00"},
+    // ... more products ...
+  ]
